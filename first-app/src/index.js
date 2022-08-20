51fa82d057be6a2f2e48176e6ba2d0d9e932c9d7
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-08-20 16:18:07 +0530

    component : props

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Cities } from './adv/loops';
-// import { ProgressBar } from './pb/progress';
 import './index.css'
+import { Mobile } from './props/mobile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Cities/>);
+
+const cc = 'IN'
+const number = '123456'
+
+const dt = { 
+    cc : 'IN',
+    number : '123456789'
+}
+
+const mob = (
+    <Mobile {...dt} >
+        <div>
+            <h1> hello </h1>
+        </div>
+    </Mobile>
+)
+
+root.render(mob);
---
+++ first-app/src/props/mobile.js
@@ -0,0 +1,13 @@
+export function Mobile({ cc, number }) {
+
+    cc = 'JP'
+    
+    return(
+        <>
+            <h1>
+                Number is {number}, {cc}
+            </h1>
+            {/* {props.children} */}
+        </>
+    )
+}
