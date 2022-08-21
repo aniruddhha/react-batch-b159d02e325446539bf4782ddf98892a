655ec6d2be6a8b81c90dea31bc5fa92ac0e0dee3
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-08-21 09:45:54 +0530

    events : basics of the evnets

---
+++ day-2/data-events-app/src/events/basics.js
@@ -0,0 +1,48 @@
+import { useState } from "react"
+import styles from './basics.module.css'
+
+export function Basic() {
+
+    const [dt, setDt] = useState('calculating')
+    const [rgb, setRgb] = useState({ 
+        r : 0,
+        g : 0,
+        b : 0
+      })
+
+    const onBtnClk = () => {
+        setDt(Date.now())
+        console.log(`clicked ${dt}`)
+    }
+
+    const onMsEnt = () => {
+
+        const r = Math.random() * 256
+        const g = Math.random() * 256
+        const b = Math.random() * 256
+
+       setRgb({ r,  g , b  })
+    }
+
+    return(
+        <>
+            <div>
+                <h1>{dt}</h1>
+            </div>
+            <div>
+                <input type='button' value ='Okay' onClick={onBtnClk} />
+            </div>
+            <div>
+                <input type='button' value ='Okay' onClick={ () => console.log(`clicked 2`)} />
+            </div>
+            <div className={styles.container}>
+                <div className={styles.box} style={{ backgroundColor: `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})` }} onMouseEnter={onMsEnt}></div>
+                <div className={styles.box} style={{ backgroundColor: `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})` }} onMouseEnter={onMsEnt}></div>
+                <div className={styles.box} style={{ backgroundColor: `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})` }} onMouseEnter={onMsEnt}></div>
+                <div className={styles.box} style={{ backgroundColor: `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})` }} onMouseEnter={onMsEnt}></div>
+                <div className={styles.box} style={{ backgroundColor: `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})` }} onMouseEnter={onMsEnt}></div>
+                <div className={styles.box} style={{ backgroundColor: `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})` }} onMouseEnter={onMsEnt}></div>
+            </div>
+        </>
+    )
+}
---
+++ day-2/data-events-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Basic } from './events/basics';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const content = <Basic/>
+
+root.render(content);
+
