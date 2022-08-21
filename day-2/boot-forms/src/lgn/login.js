3ce34b73453a2abf94ba81fa1316c972c17b56ca
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-08-21 16:52:49 +0530

    form : form validation

--- day-2/boot-forms/src/lgn/login.js
+++ day-2/boot-forms/src/lgn/login.js
@@ -1,57 +1,83 @@
 import { useState, useEffect } from 'react'
 
 export function Login() {
+    const [user, setUser] = useState({ lnm: '', fnm: '', cty: '', mob: '' })
+    const [err, setErr] = useState({ })
 
-    const [fnm, setFnm] = useState('')
-    const [lnm, setLnm] = useState('')
-    const [cty, setCty] = useState('')
-    const [mob, setMob] = useState('')
+    const onCtrlCh = e => {
 
-    const onFnmChanged = e => setFnm(e.target.value)
-    const onLnmChanged = e => setLnm(e.target.value)
-    const onCtyChanged = e => setCty(e.target.value)
-    const onMobChanged = e => setMob(e.target.value)
+        const { name, value } = e.target
 
-    useEffect(() => {   
-        console.log(`First Name ${fnm}`)
-        console.log(`Last Name ${lnm}`)
-        console.log(`City ${cty}`)
-        console.log(`Mobile ${mob}`)
-    }, [fnm, lnm, cty, mob])
+        if (name == 'fnm') {
+            if (value.length <= 3) {
+                setErr({ ...err, fnm: 'Length Should Greater than 3' })
+            }
+            else { setErr({ ...err, fnm: undefined }) }
+        }
+        if (name == 'lnm') {
+            if (value.length <= 3) { setErr({ ...err, lnm: 'Length Should Greater than 3' }) }
+            else { setErr({ ...err, lnm: undefined }) }
+        }
+        if (name == 'cty') {
+            if (value.length <= 3) { setErr({ ...err, cty: 'Length Should Greater than 3' }) }
+            else { setErr({ ...err, cty: undefined }) }
+        }
+        if (name == 'mob') {
+            if (value.length <= 3) { setErr({ ...err, mob: 'Length Should Greater than 3' }) }
+            else { setErr({ ...err, mob: undefined }) }
+        }
+
+        setUser({ ...user, [e.target.name]: e.target.value })
+    
+    }
+
+    useEffect(() => {
+        console.log(user)
+    }, [user])
 
     const onLogin = ev => {
+        console.log(ev)
         console.log(`Form Submitted`)
         ev.preventDefault()
         console.log(ev)
 
         console.log(`1. First Name ${ev.target[0].value}`)
-    
+
         console.log(`2. First Name ${ev.target.fnm.value}`)
 
         console.log(`3. First Name ${ev.target.elements.fnm.value}`)
+
+        if(Object.values(err).some(el => el == undefined))
+            console.log('Form Invalid')
+        else console.log('Form Valid')
+            
     }
 
     return (
         <form className='container' onSubmit={onLogin}>
             <div className="row g-3 mt-2">
                 <div className="col">
-                    <input type="text" name="fnm" className="form-control" placeholder="First name" value={fnm} onChange={onFnmChanged} />
+                    <input type="text" name="fnm" className="form-control" placeholder="First name" value={user.fnm} onChange={onCtrlCh} />
+                    <label>{err.fnm && err.fnm}</label>
                 </div>
                 <div className="col">
-                    <input type="text" name="lnm" className="form-control" placeholder="Last name" value={lnm} onChange={onLnmChanged}  />
-                </div>
-            </div>
-            <div className="row g-3 mt-2">
-                <div className="col">
-                    <input type="text" name="cty" className="form-control" placeholder="City" value={cty} onChange={onCtyChanged}  />
-                </div>
-                <div className="col">
-                    <input type="text" name="mob" className="form-control" placeholder="Mobile" value={mob} onChange={onMobChanged}/>
+                    <input type="text" name="lnm" className="form-control" placeholder="Last name" value={user.lnm} onChange={onCtrlCh} />
+                    <label>{err.lnm && err.lnm}</label>
                 </div>
             </div>
             <div className="row g-3 mt-2">
                 <div className="col">
-                    <input type="submit" className="btn btn-success"/>
+                    <input type="text" name="cty" className="form-control" placeholder="City" value={user.cty} onChange={onCtrlCh} />
+                    <label>{err.cty && err.cty}</label>
+                </div>
+                <div className="col">
+                    <input type="text" name="mob" className="form-control" placeholder="Mobile" value={user.mob} onChange={onCtrlCh} />
+                    <label>{err.mob && err.mob}</label>
+                </div>
+            </div>
+            <div className="row g-3 mt-2">
+                <div className="col">
+                    <input type="submit" className="btn btn-success" />
                 </div>
             </div>
         </form>
